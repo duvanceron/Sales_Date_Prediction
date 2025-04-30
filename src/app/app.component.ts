6d3701f0eea76771082f531cc4a64fe389e75a8d
duvanceron <duvanceroon@hotmail.com>
2025-04-30 08:58:36 -0500

    it is adding the routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "./shared/menu/menu.component";
 import { RouterOutlet } from '@angular/router'; //  Import router
-import { RouterModule } from '@angular/router';  // Import RouterModule for routing
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [  MenuComponent,RouterOutlet, RouterModule],
+  imports: [  MenuComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,10 +1,12 @@
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 @Component({
   selector: 'app-menu',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
